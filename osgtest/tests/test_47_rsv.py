eeea0d330a5d07d2279b54f37d671060637bea71
Scot Kronenfeld <kronenfe@cs.wisc.edu>
2012-03-28 17:02:22 +0000

    Added a couple more simple RSV tests.
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-test/trunk@14510 4e558342-562e-0410-864c-e07659590f8c

--- osgtest/tests/test_47_rsv.py
+++ osgtest/tests/test_47_rsv.py
@@ -5,7 +5,7 @@ import unittest
 
 class TestRSV(unittest.TestCase):
 
-    def test_01_rsv_version(self):
+    def test_001_version(self):
         if core.missing_rpm('rsv'):
             return
 
@@ -14,3 +14,26 @@ class TestRSV(unittest.TestCase):
 
         # The rsv-control --version just returns a string like '1.0.0'.
         self.assert_(re.search('\d.\d.\d', stdout) is not None)
+
+
+    def test_010_list(self):
+        if core.missing_rpm('rsv'):
+            return
+
+        command = ('rsv-control', '--list', '--all')
+        stdout = core.check_system(command, 'rsv-control --list --all')[0]
+
+        # I don't want to parse the output too much, but we know that most
+        # of the metrics start with 'org.osg.'.  So just check for that string
+        # once and we'll call it good enough.
+        self.assert_(re.search('org.osg.', stdout) is not None)
+
+    def test_011_list_with_cron(self):
+        if core.missing_rpm('rsv'):
+            return
+        
+        command = ('rsv-control', '--list', '--all', '--cron')
+        stdout = core.check_system(command, 'rsv-control --list --all')[0]
+
+        # One of the header columns will say 'Cron times'
+        self.assert_(re.search('Cron times', stdout) is not None)
