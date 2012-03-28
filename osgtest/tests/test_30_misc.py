8d20a00d248d1ccffcdc40cd59ffb45a2e31225b
Alain Roy <roy@cs.wisc.edu>
2012-03-28 18:39:46 +0000

    Added a really basic test of osg-version to make sure that it reports a version number and that the version number matches the version number in the RPM. It's not a very important test, but its my first test, to learn the basics of writing a test.
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-test/trunk@14516 4e558342-562e-0410-864c-e07659590f8c

--- osgtest/tests/test_30_misc.py
+++ osgtest/tests/test_30_misc.py
@@ -2,7 +2,7 @@ import osgtest.library.core as core
 import re
 import unittest
 
-class TestNDT(unittest.TestCase):
+class TestMisc(unittest.TestCase):
 
     def test_01_web100clt(self):
         if core.missing_rpm('ndt'):
@@ -11,3 +11,30 @@ class TestNDT(unittest.TestCase):
         stdout = core.check_system(command, 'NDT client')[0]
         self.assert_(re.search('ndt.+version', stdout, re.IGNORECASE)
                      is not None)
+
+    def test_02_osg_version(self):
+        if core.missing_rpm('osg-version'):
+            return
+        command = ('osg-version',)
+        
+        # First we verify that osg-version runs
+        stdout = core.check_system(command, 'osg-version')[0]
+        
+        # Then we pull out the version number from the output
+        version_pattern = re.compile(r'(\d+\.\d+\.\d+)')
+        matches = version_pattern.search(stdout)
+
+        # Is there a version number?
+        self.assert_(matches is not None)
+        osg_version = matches.group(1)
+
+        # Get the version number from the RPM
+        command = ('rpm', '-q', 'osg-version')
+        stdout = core.check_system(command, "osg-version RPM version")[0]
+        matches = version_pattern.search(stdout)
+        self.assert_(matches is not None)
+
+        # Verify that the versions match
+        osg_version_rpm_version = matches.group(1)
+        self.assert_(osg_version == osg_version_rpm_version)
+        
