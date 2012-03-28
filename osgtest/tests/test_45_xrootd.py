e6c62f48f0ad5f26954b1ff306fc88bc4e130af2
Doug Strain <dstrain@fnal.gov>
2012-03-28 20:46:54 +0000

    OSG Test: adding a xrootd-fuse test based on os.system() and fuse xrootdfs mount
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-test/trunk@14521 4e558342-562e-0410-864c-e07659590f8c

--- osgtest/tests/test_45_xrootd.py
+++ osgtest/tests/test_45_xrootd.py
@@ -8,6 +8,7 @@ import unittest
 class TestXrootd(unittest.TestCase):
 
     __data_path = '/usr/share/osg-test/test_gridftp_data.txt'
+    __fuse_path = '/mnt/xrootd_fuse_test'
 
     def test_01_xrdcp_local_to_server(self):
         if core.missing_rpm('xrootd-server', 'xrootd-client'):
@@ -36,9 +37,9 @@ class TestXrootd(unittest.TestCase):
         temp_target_dir = tempfile.mkdtemp()
         os.chmod(temp_source_dir, 0777)
         os.chmod(temp_target_dir, 0777)
-	f=open(temp_source_dir+"/copied_file.txt","w")
-	f.write("This is some test data for an xrootd test.")
-	f.close()
+        f=open(temp_source_dir+"/copied_file.txt","w")
+        f.write("This is some test data for an xrootd test.")
+        f.close()
         xrootd_url = 'root://%s/%s/copied_file.txt' % (hostname, temp_source_dir)
         local_path = temp_target_dir + '/copied_file.txt'
         command = ('xrdcp', xrootd_url, local_path)
@@ -51,3 +52,37 @@ class TestXrootd(unittest.TestCase):
         files.remove(temp_target_dir)
         self.assertEqual(status, 0, fail)
         self.assert_(file_copied, 'Copied file missing')
+
+    def test_03_xrootd_fuse(self):
+        """ This tests xrootd-fuse using a mount in /mnt """
+        if core.missing_rpm('xrootd-server', 'xrootd-client','xrootd-fuse'):
+            return
+        if not os.path.exists("/mnt"):
+            log_message("/mnt did not exist, skipping xrootd fuse test")
+        if not os.path.exists(TestXrootd.__fuse_path):
+            os.mkdir(TestXrootd.__fuse_path)
+        hostname = socket.getfqdn()
+        #command = ('xrootdfs',TestXrootd.__fuse_path,'-o','rdr=xroot://localhost:1094//tmp','-o','uid=xrootd')
+        command = ('mount', '-t','fuse','-o','rdr=xroot://localhost:1094//tmp,uid=xrootd','xrootdfs',TestXrootd.__fuse_path)
+        command_str= ' '.join(command)
+
+        #For some reason, sub process hangs on fuse processes, use os.system
+        #status, stdout, stderr = core.system(command_str,shell=True)
+        os.system(command_str)
+       
+        # Copy a file in and see if it made it into the fuse mount
+        xrootd_url = 'root://%s/%s/copied_file.txt' % (hostname, "/tmp")
+        command = ('xrdcp', TestXrootd.__data_path , xrootd_url)
+        status, stdout, stderr = core.system(command, True)
+       
+        command = ('ls', "/tmp/copied_file.txt")
+        stdout, stderr, fail = core.check_system(command, "Checking file is copied to xrootd fuse mount correctly", user=True)
+
+
+        command = ('umount',TestXrootd.__fuse_path)
+        status, stdout, stderr = core.system(command)
+        os.rmdir(TestXrootd.__fuse_path)
+        files.remove("/tmp/copied_file.txt")
+
+
+
