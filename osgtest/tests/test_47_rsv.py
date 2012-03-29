f38585ea8fe2614c21525db7a4330a86f9f93cce
Scot Kronenfeld <kronenfe@cs.wisc.edu>
2012-03-29 02:45:03 +0000

    Added some more tests for RSV (--on, --off, --job-list)
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-test/trunk@14526 4e558342-562e-0410-864c-e07659590f8c

--- osgtest/tests/test_47_rsv.py
+++ osgtest/tests/test_47_rsv.py
@@ -5,6 +5,21 @@ import unittest
 
 class TestRSV(unittest.TestCase):
 
+    def start_rsv(self):
+        core.check_system(('rsv-control', '--on'), 'rsv-control --on')
+        return
+    
+    def stop_rsv(self):
+        core.check_system(('rsv-control', '--off'), 'rsv-control --off')
+        return
+    
+    def config_and_restart(self):
+        self.stop_rsv()
+        core.check_system(('osg-configure', '-c', '-m', 'rsv'), 'osg-configure -c -m rsv')
+        self.start_rsv()
+        return
+
+
     def test_001_version(self):
         if core.missing_rpm('rsv'):
             return
@@ -37,3 +52,32 @@ class TestRSV(unittest.TestCase):
 
         # One of the header columns will say 'Cron times'
         self.assert_(re.search('Cron times', stdout) is not None)
+
+
+    def test_020_start_and_stop(self):
+        if core.missing_rpm('rsv'):
+            return
+
+        self.stop_rsv()
+        self.start_rsv()
+
+    def test_021_job_list(self):
+        if core.missing_rpm('rsv'):
+            return
+
+        command = ('rsv-control', '--job-list')
+        stdout = core.check_system(command, 'rsv-control --job-list')[0]
+
+        # TODO
+        # Make sure that the header prints at least.  We can improve this
+        self.assert_(re.search('OWNER', stdout) is not None)
+
+
+        # Check the parsable job-list output
+        command = ('rsv-control', '--job-list', '--parsable')
+        stdout = core.check_system(command, 'rsv-control --job-list --parsable')[0]
+
+        # The separator is a pipe, so just make sure we got one of those
+        self.assert_(re.search('\|', stdout) is not None)
+
+        return
