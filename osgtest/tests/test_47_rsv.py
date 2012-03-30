80bdc3c2191f5cf98853c4a70a67f015c5251bfc
Scot Kronenfeld <kronenfe@cs.wisc.edu>
2012-03-30 18:34:52 +0000

    Added more to the RSV testing, including:  - creating rsvcert and rsvkey  - writing into the grid-mapfile (this needs some more work pending    an API change from Tim regarding backing up and restoring files)  - running a metric that depends on Globus
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-test/trunk@14541 4e558342-562e-0410-864c-e07659590f8c

--- osgtest/tests/test_47_rsv.py
+++ osgtest/tests/test_47_rsv.py
@@ -1,9 +1,22 @@
 import osgtest.library.core as core
 
 import re
+import os
+import pwd
+import shutil
 import socket
 import unittest
 import ConfigParser
+
+"""
+Testing number conventions:
+ 001-009 - Configuration, making proxy, etc
+ 010-019 - Tests that don't require the rsv service to be running
+ 020-029 - Tests for the tools that require the rsv service to be running
+ 030-049 - Testing of metrics that don't require any other services
+ 050-079 - Testing of metrics that require a CE
+ 080-099 - Testing of metrics that require an SE
+"""
 
 core.config['rsv.config-file'] = '/etc/osg/config.d/30-rsv.ini'
 
@@ -32,9 +45,56 @@ class TestRSV(unittest.TestCase):
 
         self.assert_(re.search('metricStatus: OK', stdout) is not None)
         return
-    
 
-    def test_001_version(self):
+
+    def test_001_set_config_vals(self):
+        core.config['rsv.certfile'] = "/etc/grid-security/rsv/rsvcert.pem"
+        core.config['rsv.keyfile'] = "/etc/grid-security/rsv/rsvkey.pem"
+        
+    def test_002_setup_certificate(self):
+        if core.missing_rpm('rsv'):
+            return
+
+        # TODO - on fermicloud machines we copy the hostcert.  Can we do better?
+        if not os.path.exists(os.path.dirname(core.config['rsv.certfile'])):
+            os.makedirs(os.path.dirname(core.config['rsv.certfile']))
+        if not os.path.exists(core.config['rsv.certfile']):
+            shutil.copy('/etc/grid-security/hostcert.pem', core.config['rsv.certfile'])
+        if not os.path.exists(core.config['rsv.keyfile']):
+            shutil.copy('/etc/grid-security/hostkey.pem', core.config['rsv.keyfile'])
+
+        (rsv_uid, rsv_gid) = pwd.getpwnam('rsv')[2:4]
+        os.chown(core.config['rsv.certfile'], rsv_uid, rsv_gid)
+        os.chmod(core.config['rsv.certfile'], 0444)
+        os.chown(core.config['rsv.keyfile'], rsv_uid, rsv_gid)
+        os.chmod(core.config['rsv.keyfile'], 0400)
+        return
+
+
+    def test_003_setup_grid_mapfile(self):
+        # Register the cert in the gridmap file
+        cert_subject = core.certificate_info(core.config['rsv.certfile'])[0]
+
+        # TODO - we should track this file and restore the original grid mapfile
+        fd = open('/etc/grid-security/grid-mapfile', 'a')
+        fd.write('"%s" rsv' % (cert_subject))
+        fd.close()
+
+
+    def test_004_load_default_config(self):
+        if core.missing_rpm('rsv'):
+            return
+
+        # We'll pull in the default config file and store it.  We might want to
+        # do tests based on the default.
+        self.config = ConfigParser.RawConfigParser()
+        self.config.optionxform = str
+        self.config.read(core.config['rsv.config-file'])
+        core.config['rsv.default-config'] = self.config
+        return
+
+
+    def test_010_version(self):
         if core.missing_rpm('rsv'):
             return
 
@@ -43,9 +103,10 @@ class TestRSV(unittest.TestCase):
 
         # The rsv-control --version just returns a string like '1.0.0'.
         self.assert_(re.search('\d.\d.\d', stdout) is not None)
+        return
 
 
-    def test_002_list(self):
+    def test_011_list(self):
         if core.missing_rpm('rsv'):
             return
 
@@ -56,8 +117,10 @@ class TestRSV(unittest.TestCase):
         # of the metrics start with 'org.osg.'.  So just check for that string
         # once and we'll call it good enough.
         self.assert_(re.search('org.osg.', stdout) is not None)
+        return
 
-    def test_003_list_with_cron(self):
+
+    def test_012_list_with_cron(self):
         if core.missing_rpm('rsv'):
             return
         
@@ -66,18 +129,17 @@ class TestRSV(unittest.TestCase):
 
         # One of the header columns will say 'Cron times'
         self.assert_(re.search('Cron times', stdout) is not None)
+        return
 
 
-    def test_010_load_default_config(self):
+    def test_013_profiler(self):
         if core.missing_rpm('rsv'):
             return
-
-        # We'll pull in the default config file and store it.  We might want to
-        # do tests based on the default.
-        self.config = ConfigParser.RawConfigParser()
-        self.config.optionxform = str
-        self.config.read(core.config['rsv.config-file'])
-        core.config['rsv.default-config'] = self.config
+        
+        command = ('rsv-control', '--profile')
+        stdout = core.check_system(command, 'rsv-control --profile')[0]
+        self.assert_(re.search('Running the rsv-profiler', stdout) is not None)
+        self.assert_(os.path.exists('rsv-profiler.tar.gz'))
         return
 
 
@@ -99,7 +161,12 @@ class TestRSV(unittest.TestCase):
         # TODO
         # Make sure that the header prints at least.  We can improve this
         self.assert_(re.search('OWNER', stdout) is not None)
+        return
 
+
+    def test_021_job_list_parsable(self):
+        if core.missing_rpm('rsv'):
+            return
 
         # Check the parsable job-list output
         command = ('rsv-control', '--job-list', '--parsable')
@@ -107,7 +174,6 @@ class TestRSV(unittest.TestCase):
 
         # The separator is a pipe, so just make sure we got one of those
         self.assert_(re.search('\|', stdout) is not None)
-
         return
 
 
@@ -119,10 +185,23 @@ class TestRSV(unittest.TestCase):
         return
 
 
-    def test_031_hostcert_expiry(self):
+    def test_031_hostcert_expiry_metric(self):
         if core.missing_rpm('rsv'):
             return
 
         self.run_metric('org.osg.local.hostcert-expiry')
         return
-        
+
+
+    def test_050_gram_authentication_metric(self):
+        if core.missing_rpm('rsv', 'globus-gatekeeper'):
+            return
+
+        self.run_metric('org.osg.globus.gram-authentication')
+        return
+
+# Test to write:
+# - run html-consumer
+# - test that html page is created
+# - run gratia-consumer?
+# - run Globus metrics
