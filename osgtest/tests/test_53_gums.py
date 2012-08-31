bc8836d8c4ae62c30c88d8a8632efe09e2679013
Tim Cartwright <cat@cs.wisc.edu>
2012-08-31 21:01:26 +0000

    Initial GUMS usage test.
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-test/trunk@15483 4e558342-562e-0410-864c-e07659590f8c

---
+++ osgtest/tests/test_53_gums.py
@@ -0,0 +1,19 @@
+import os
+import unittest
+
+import osgtest.library.core as core
+import osgtest.library.files as files
+import osgtest.library.tomcat as tomcat
+
+class TestGUMS(unittest.TestCase):
+
+    def test_01_map_user(self):
+        if core.missing_rpm('gums-service'):
+            return
+
+        host_dn, _ = core.certificate_info(core.config['certs.hostcert'])
+        pwd_entry = pwd.getpwnam(core.options.username)
+        cert_path = os.path.join(pwd_entry.pw_dir, '.globus', 'usercert.pem')
+        user_dn, _ = core.certificate_info(cert_path)
+        command = ('gums', 'mapUser', '--service', host_dn, user_dn)
+        core.check_system(command, 'Map GUMS user')
