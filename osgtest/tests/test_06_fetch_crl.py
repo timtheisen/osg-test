ba4c7afb80ccd068bbe52b4e5ca35a6f29529020
Tim Cartwright <cat@cs.wisc.edu>
2012-12-14 22:12:35 +0000

    Almost complete overhaul of the Fetch CRL tests; (a) to prepare for the case in which the name of the fetch-crl* package must be determined at runtime (expected for el5, soonish), and (b) to check stdout for acceptable failure messages (e.g., CRL download failures) that we do not care about.
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-test/trunk@15978 4e558342-562e-0410-864c-e07659590f8c

--- osgtest/tests/test_06_fetch_crl.py
+++ osgtest/tests/test_06_fetch_crl.py
@@ -1,30 +1,81 @@
+import glob
 import os
-import osgtest.library.core as core
-import unittest
+import re
+import shutil
 import tempfile
+import unittest
+
+import osgtest.library.core as core
 
 class TestFetchCrl(unittest.TestCase):
-    def test_01_fetch_crl(self):
-        if core.missing_rpm('fetch-crl'):
+
+    error_message_whitelists = {
+        '2': (
+        ),
+        '3': (
+            'CRL has nextUpdate time in the past',
+            'CRL verification failed for',
+            'Download error'
+        )
+    }
+
+    def output_is_acceptable(self, stdout):
+        whitelist = TestFetchCrl.error_message_whitelists[core.config['fetch-crl.major-version']]
+        all_lines_ok = True
+        for line in stdout.rstrip('\n').split('\n'):
+            line_ok = False
+            for error_string in whitelist:
+                if error_string in line:
+                    line_ok = True
+                    break
+            if not line_ok:
+                all_lines_ok = False
+                break
+        return all_lines_ok
+
+    def test_01_determine_package_name(self):
+        core.config['fetch-crl.package'] = None
+        core.config['fetch-crl.version'] = None
+        core.config['fetch-crl.major-version'] = None
+
+        if core.rpm_is_installed('fetch-crl3'):
+            core.config['fetch-crl.package'] = 'fetch-crl3'
+        elif core.rpm_is_installed('fetch-crl'):
+            core.config['fetch-crl.package'] = 'fetch-crl'
+        else:
             return
-        command=['fetch-crl']
-        stdout, _, fail = core.check_system(command, 'Start fetch-crl')
-        count=0
-        for name in os.listdir("/etc/grid-security/certificates"):
-            if name[-2:] == "r0":
-                count=count+1
-        self.assert_(count>3,True)
-    def test_02_fetch_crl_dir(self):
-        if core.missing_rpm('fetch-crl'):
+
+        command = ('rpm', '--query', core.config['fetch-crl.package'])
+        stdout = core.check_system(command, 'Get fetch-crl package NVR')[0]
+        matches = re.match(r'fetch-crl[^-]*-(([^-])[^-]*)-[^-]+', stdout)
+        self.assert_(matches is not None, 'Get %s NVR' % core.config['fetch-crl.package'])
+        core.config['fetch-crl.version'] = matches.group(1)
+        core.config['fetch-crl.major-version'] = matches.group(2)
+
+    def test_02_fetch_crl(self):
+        if core.config['fetch-crl.package'] is None:
+            core.skip('Fetch CRL is not installed')
             return
-        tmpdir=tempfile.mkdtemp()
-        command=('fetch-crl','-o',tmpdir)
-        stdout, _, fail = core.check_system(command, 'Start fetch-crl with a output dir')
-        count=0
-        for name in os.listdir(tmpdir):
-            if name[-2:] == "r0":
-                count=count+1
-            os.unlink(os.path.join(tmpdir,name))
-        os.rmdir(tmpdir)
-        self.assert_(count>3,True)
-        
+        command = [core.config['fetch-crl.package']]
+        status, stdout, stderr = core.system(command)
+        fail = core.diagnose('Run %s in /etc' % core.config['fetch-crl.package'], status, stdout, stderr)
+        self.assert_(0 <= status <= 1, fail)
+        if status == 1:
+            self.assert_(self.output_is_acceptable(stdout), fail)
+        count = len(glob.glob(os.path.join('/etc/grid-security/certificates', '*.r[0-9]')))
+        self.assert_(count > 3, True)
+
+    def test_03_fetch_crl_dir(self):
+        if core.config['fetch-crl.package'] is None:
+            core.skip('Fetch CRL is not installed')
+            return
+        temp_crl_dir = tempfile.mkdtemp()
+        command = (core.config['fetch-crl.package'], '-o', temp_crl_dir)
+        status, stdout, stderr = core.system(command)
+        fail = core.diagnose('Run %s in temp dir' % core.config['fetch-crl.package'], status, stdout, stderr)
+        self.assert_(0 <= status <= 1, fail)
+        if status == 1:
+            self.assert_(self.output_is_acceptable(stdout), fail)
+        count = len(glob.glob(os.path.join(temp_crl_dir, '*.r[0-9]')))
+        shutil.rmtree(temp_crl_dir)
+        self.assert_(count > 3, True)
