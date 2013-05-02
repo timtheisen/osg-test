87fe0924c5281fbff38b7363a285d462dc99c9a0
Srivinasan Ramachandran <sriniram26@gmail.com>
2013-05-02 16:24:14 +0000

    First version. More tests would be added later...
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-test/branches/SriniGratia@16728 4e558342-562e-0410-864c-e07659590f8c

---
+++ osgtest/tests/test_54_gratia.py
@@ -0,0 +1,11 @@
+import osgtest.library.core as core
+import osgtest.library.osgunittest as osgunittest
+
+class TestGratia(osgunittest.OSGTestCase):
+
+    def test_01_gratia_admin_webpage (self):
+        core.skip_ok_unless_installed('gratia-service')
+
+        command = ('curl', ' http://gratia-integration1.fnal.gov/gratia-administration/status.html?wantDetails=0')
+        status, stdout, stderr = core.system(command)
+        self.assertNotEqual(status, 0, 'Unable to launch gratia admin webpage')
