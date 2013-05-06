f22e43c4397a647cbba329cd8027ad6f1c26668c
Srivinasan Ramachandran <sriniram26@gmail.com>
2013-05-06 18:02:51 +0000

    Fixed command syntax
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-test/branches/SriniGratia@16746 4e558342-562e-0410-864c-e07659590f8c

--- osgtest/tests/test_54_gratia.py
+++ osgtest/tests/test_54_gratia.py
@@ -4,8 +4,8 @@ import osgtest.library.osgunittest as osgunittest
 class TestGratia(osgunittest.OSGTestCase):
 
     def test_01_gratia_admin_webpage (self):
+        
         core.skip_ok_unless_installed('gratia-service')
-
-        command = ('curl', ' http://gratia-integration1.fnal.gov/gratia-administration/status.html?wantDetails=0')
-        status, stdout, stderr = core.system(command)
+        command = "curl http://gratia-integration1.fnal.gov/gratia-administration/status.html?wantDetails=0"
+        status, stdout, stderr = core.system(command, shell=True)
         self.assertEqual(status, 0, 'Unable to launch gratia admin webpage')
