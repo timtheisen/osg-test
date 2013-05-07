9d750467a54112a5cc371d9649ed904b1824c558
Srivinasan Ramachandran <sriniram26@gmail.com>
2013-05-07 20:50:50 +0000

    added some print statements
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-test/branches/SriniGratia@16752 4e558342-562e-0410-864c-e07659590f8c

--- osgtest/tests/test_54_gratia.py
+++ osgtest/tests/test_54_gratia.py
@@ -4,8 +4,10 @@ import osgtest.library.osgunittest as osgunittest
 class TestGratia(osgunittest.OSGTestCase):
 
     def test_01_gratia_admin_webpage (self):
-        
+         
         core.skip_ok_unless_installed('gratia-service')
-        command = ('curl', 'http://gratia-integration1.fnal.gov/gratia-administration/status.html?wantDetails=0')
+        command = ('curl', 'http://fermicloud316.fnal.gov:8880/gratia-administration/status.html?wantDetails=0')
         status, stdout, stderr = core.system(command)
+        print "stdout is: " + str(stdout)
+        print "stderr is: " + str(stderr)
         self.assertEqual(status, 0, 'Unable to launch gratia admin webpage')
