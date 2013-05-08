e63b02d78676af60dd75e90060002a6516d50445
Srivinasan Ramachandran <sriniram26@gmail.com>
2013-05-08 15:26:12 +0000

    Added show_gratia_database_tables test case
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-test/branches/SriniGratia@16768 4e558342-562e-0410-864c-e07659590f8c

--- osgtest/tests/test_54_gratia.py
+++ osgtest/tests/test_54_gratia.py
@@ -10,8 +10,8 @@ class TestGratia(osgunittest.OSGTestCase):
         core.skip_ok_unless_installed('gratia-service')
         command = ('curl', 'http://fermicloud316.fnal.gov:8880/gratia-administration/status.html?wantDetails=0')
         status, stdout, stderr = core.system(command)
-        print "stdout is: " + str(stdout)
-        print "stderr is: " + str(stderr)
+        #print "stdout is: " + str(stdout)
+        #print "stderr is: " + str(stderr)
         self.assertEqual(status, 0, 'Unable to launch gratia admin webpage')
         
     def test_02_show_databases(self):
@@ -24,13 +24,36 @@ class TestGratia(osgunittest.OSGTestCase):
         f.write("password=admin\n")
         f.close()
         
-        #Command to drop the gratia database is:
+        #Command to show the databases is:
         #echo "show databases;" | mysql --defaults-extra-file="/tmp/gratia_admin_pass.<pid>.txt" -B --unbuffered  --user=root --port=3306         
         command = "echo \"show databases;\" | mysql --defaults-extra-file=\"" + filename + "\" -B --unbuffered  --user=root --port=3306 | wc -l",
         status, stdout, stderr = core.system(command, shell=True)
         self.assertEqual(status, 0, 'Unable to install Gratia Database !')
         #self.assertEqual(stdout, 5, 'Incorrect total number of databases !')
-        print "stdout is: " + stdout
+        print "show_databases stdout is: " + stdout
         result = re.search('5', stdout, re.IGNORECASE)
         self.assert_(result is not None)
         os.remove(filename)
+        
+    def test_03_show_gratia_database_tables(self):
+        core.skip_ok_unless_installed('gratia-service')    
+       
+        filename = "/tmp/gratia_admin_pass." + str(os.getpid()) + ".txt"
+        #print filename
+        f = open(filename,'w')
+        f.write("[client]\n")
+        f.write("password=admin\n")
+        f.close()
+        
+        #Command to show the tabes in the gratia database is:
+        #echo "use gratia;show tables;" | mysql --defaults-extra-file="/tmp/gratia_admin_pass.<pid>.txt" -B --unbuffered  --user=root --port=3306         
+        command = "echo \"use gratia;show tables;\" | mysql --defaults-extra-file=\"" + filename + "\" -B --unbuffered  --user=root --port=3306 | wc -l",
+        status, stdout, stderr = core.system(command, shell=True)
+        self.assertEqual(status, 0, 'Unable to install Gratia Database !')
+        #self.assertEqual(stdout, 5, 'Incorrect total number of databases !')
+        print "show_gratia_database_tables stdout is: " + stdout
+        result = re.search('82', stdout, re.IGNORECASE)
+        self.assert_(result is not None)
+        os.remove(filename)
+        
+        
