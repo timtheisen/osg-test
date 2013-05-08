1ed87ae6e00da54bcfd657474a1cdb5be4d7e2b6
Srivinasan Ramachandran <sriniram26@gmail.com>
2013-05-08 21:03:17 +0000

    First draft of execute_gridftptransfer_probedriver test cases
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-test/branches/SriniGratia@16774 4e558342-562e-0410-864c-e07659590f8c

--- osgtest/tests/test_54_gratia.py
+++ osgtest/tests/test_54_gratia.py
@@ -1,7 +1,11 @@
+from distutils.sysconfig import get_python_lib
 import osgtest.library.core as core
 import osgtest.library.osgunittest as osgunittest
 import os
 import re
+import test_28_gratia.TestStartGratia.patternreplace
+import shutil
+
 
 class TestGratia(osgunittest.OSGTestCase):
 
@@ -55,5 +59,35 @@ class TestGratia(osgunittest.OSGTestCase):
         result = re.search('82', stdout, re.IGNORECASE)
         self.assert_(result is not None)
         os.remove(filename)
+        
+    def test_04_modify_gridftptransfer_probeconfig(self):
+        core.skip_ok_unless_installed('gratia-service', 'gratia-probe-gridftp-transfer')
+        probeconfig = "/etc/gratia/gridftp-transfer/ProbeConfig"
+        self.patternreplace(probeconfig, "CollectorHost", "CollectorHost=\"fermicloud316.fnal.gov:8880\"")
+        self.patternreplace(probeconfig, "SSLHost", "SSLHost=\"fermicloud316.fnal.gov:8443\"")
+        self.patternreplace(probeconfig, "SSLRegistrationHost", "SSLRegistrationHost=\"fermicloud316.fnal.gov:8880\"")
+        self.patternreplace(probeconfig, "SiteName", "SiteName=\"OSG Test site\"")
+        self.patternreplace(probeconfig, "SiteName", "EnableProbe=\"1\"")
+        
+    def test_05_copy_gridftp_logs(self):
+        core.skip_ok_unless_installed('gratia-service', 'gratia-probe-gridftp-transfer')
+        src_dir = os.path.join(get_python_lib(), 'files')
+        dst_dir = '/var/log'
+        shutil.copyfile(src_dir, dst_dir)
+    
+    def test_06_execute_gridftptransfer_probedriver(self):
+        core.skip_ok_unless_installed('gratia-service', 'gratia-probe-gridftp-transfer')
+        command = ('/usr/share/gratia/gridftp-transfer/GridftpTransferProbeDriver',)
+        status, stdout, stderr = core.system(command)
+        self.assertEqual(status, 0, 'Unable to execute GridftpTransferProbeDriver!')
+        self.assert_(not os.listdir('/var/lib/gratia/tmp/gratiafiles/subdir.gridftp-transfer_fermicloud316.fnal.gov_fermicloud316.fnal.gov_8880/outbox/'), 'gridftp-transfer outbox NOT empty !')
+        core.state['gratia.gridftp-transfer-running'] = True
+        
+    def test_07_checkdatabase_gridftptransfer_probedriver(self):
+        self.skip_bad_if(core.state['gratia.gridftp-transfer-running'] == False)
+        pass
+
+        
+
         
         
