0a26fced912fa1ff031049a44b4e7e2365aa1421
Srivinasan Ramachandran <sriniram26@gmail.com>
2013-05-10 20:19:51 +0000

    Replaced many core.system with core.check_system commands and added a few comments
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-test/branches/SriniGratia@16801 4e558342-562e-0410-864c-e07659590f8c

--- osgtest/tests/test_80_gratia.py
+++ osgtest/tests/test_80_gratia.py
@@ -24,17 +24,22 @@ class TestStopGratia(osgunittest.OSGTestCase):
             if len(os.listdir(target_dir)) == 0:
                 os.rmdir(target_dir)
 
-
+    #===========================================================================
+    # This test removes the http certificates
+    #===========================================================================
     def test_01_remove_certs(self):
         # Do the keys first, so that the directories will be empty for the certs.
         self.remove_cert('certs.httpkey')
         self.remove_cert('certs.httpcert')
 
+    #===========================================================================
+    # This test drops the gratia database
+    #===========================================================================
     def test_02_uninstall_gratia_database(self):
         core.skip_ok_unless_installed('gratia-service')    
        
         filename = "/tmp/gratia_admin_pass." + str(os.getpid()) + ".txt"
-        #print filename
+        #open the above file and write admin password information on the go
         f = open(filename,'w')
         f.write("[client]\n")
         f.write("password=admin\n")
@@ -43,34 +48,37 @@ class TestStopGratia(osgunittest.OSGTestCase):
         #Command to drop the gratia database is:
         #echo "drop database gratia;" | mysql --defaults-extra-file="/tmp/gratia_admin_pass.<pid>.txt" -B --unbuffered  --user=root --port=3306         
         command = "echo \"drop database gratia;\" | mysql --defaults-extra-file=\"" + filename + "\" -B --unbuffered  --user=root --port=3306"
-        status, stdout, stderr = core.system(command, shell=True)
-        self.assertEqual(status, 0, 'Unable to install Gratia Database !')
+        core.check_system(command, 'Unable to drop Gratia Database !', shell=True)
         os.remove(filename)
         
+    #===========================================================================
+    # This test cleans up files in /var/lib/gratia/tmp/gratiafiles/
+    #===========================================================================
     def test_03_cleanup_temp_gratiafiles(self):
         core.skip_ok_unless_installed('gratia-service', 'gratia-probe-gridftp-transfer')
         command = ('rm', '-rf', '/var/lib/gratia/tmp/gratiafiles/')
-        status, _, _ = core.system(command)
-        self.assertEqual(status, 0, 'Unable to clean up /var/lib/gratia/tmp/gratiafiles/ !')
+        core.check_system(command, 'Unable to clean up /var/lib/gratia/tmp/gratiafiles/ !')
     
+    #===========================================================================
+    # This test removes /var/lib/gratia/tmp/GridftpAccountingProbeState
+    #===========================================================================
     def test_04_cleanup_temp_gridftpaccountingprobestate(self):
         core.skip_ok_unless_installed('gratia-service', 'gratia-probe-gridftp-transfer')
         command = ('rm', '-rf', '/var/lib/gratia/tmp/GridftpAccountingProbeState')
-        status, _, _ = core.system(command)
-        self.assertEqual(status, 0, 'Unable to clean up /var/lib/gratia/tmp/GridftpAccountingProbeState !')
+        core.check_system(command, 'Unable to clean up /var/lib/gratia/tmp/GridftpAccountingProbeState !')
 
+    #===========================================================================
+    # This test removes /var/log/gridftp.log
+    #===========================================================================
     def test_05_cleanup_varlog_gridftplog(self):
         core.skip_ok_unless_installed('gratia-service', 'gratia-probe-gridftp-transfer')
         command = ('rm', '-rf', '/var/log/gridftp.log')
-        status, _, _ = core.system(command)
-        self.assertEqual(status, 0, 'Unable to clean up  /var/log/gridftp.log!')
-        
+        core.check_system(command, 'Unable to clean up  /var/log/gridftp.log!')
+    
+    #===========================================================================
+    # This test removes /var/log/gridftp-auth.log
+    #===========================================================================
     def test_06_cleanup_varlog_gridftpauthlog(self):
         core.skip_ok_unless_installed('gratia-service', 'gratia-probe-gridftp-transfer')
         command = ('rm', '-rf', '/var/log/gridftp-auth.log')
-        status, _, _ = core.system(command)
-        self.assertEqual(status, 0, 'Unable to clean up  /var/log/gridftp-auth.log!')
-    
-    
-    
-        
+        core.check_system(command, 'Unable to clean up  /var/log/gridftp-auth.log!')
