34ad9310e1f28a9b13c5e9c0a64a850493f058e6
Srivinasan Ramachandran <sriniram26@gmail.com>
2013-05-14 20:14:38 +0000

    Introduced a 60s sleep for Gratia to "digest" probe info
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-test/branches/SriniGratia@16832 4e558342-562e-0410-864c-e07659590f8c

--- osgtest/tests/test_54_gratia.py
+++ osgtest/tests/test_54_gratia.py
@@ -5,6 +5,7 @@ import re
 from distutils.sysconfig import get_python_lib
 import shutil
 import socket
+import time
 
 
 class TestGratia(osgunittest.OSGTestCase):
@@ -156,12 +157,17 @@ class TestGratia(osgunittest.OSGTestCase):
         #1 row in set (0.00 sec)
         #The assertions below try to search for the numbers presented above
         
+        #Per Tanya, need to sleep for a minute or so to allow gratia to "digest" probe data
+        time.sleep(60)
+        
         command = "echo \"use gratia; select sum(Njobs) from MasterTransferSummary;\" | mysql --defaults-extra-file=\"" + filename + "\" --skip-column-names -B --unbuffered  --user=root --port=3306",
         #_, stdout, _ = core.check_system(command, 'Unable to query Gratia Database MasterTransferSummary table !', shell=True)
         status, stdout, _ = core.system(command, shell=True)
         self.assertEqual(status, 0, 'Unable to query Gratia Database MasterTransferSummary table !')
         print "select sum(Njobs) stdout is: " + stdout
         result1 = re.search('1167', stdout, re.IGNORECASE)
+        print "select sum(Njobs) type(stdout) is: " + type(stdout)
+        print "select sum(Njobs) type(result1) is: " + type(result1)
         print "select sum(Njobs) result1 is: " + str(result1)
         self.assert_(result1 is not None)
         
@@ -180,6 +186,8 @@ class TestGratia(osgunittest.OSGTestCase):
         self.assertEqual(status, 0, 'Unable to query Gratia Database MasterTransferSummary table !')
         print "select sum(TransferSize) stdout is: " + stdout
         result2 = re.search('220545414576', stdout, re.IGNORECASE)
+        print "select sum(TransferSize) type(stdout) is: " + type(stdout)
+        print "select sum(TransferSize) type(result2) is: " + type(result2)
         print "select sum(TransferSize) result2 is: " + str(result2)
         self.assert_(result2 is not None)
         
