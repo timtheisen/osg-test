7065c169e8a70d4d80324dfe6bf7cd3a6b266afc
Matyas Selmeci <matyas@cs.wisc.edu>
2013-07-03 01:33:58 +0000

    Add test for cvmfs_config probe for CVMFS 2.1 (SOFTWARE-1097)
    Since it probes the oasis.opensciencegrid.org server, I had to add the oasis-config package as a dependency.
    oasis-config is not part of osg-tested-internal, so the probe test will be skipped in the nightlies.
    (Since it was moved to a separate method though, the rest of the cvmfs tests aren't affected).
    
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-test/trunk@17120 4e558342-562e-0410-864c-e07659590f8c

--- osgtest/tests/test_38_cvmfs.py
+++ osgtest/tests/test_38_cvmfs.py
@@ -12,16 +12,42 @@ class TestCvmfs(osgunittest.OSGTestCase):
     __check_path = '/cvmfs/cms.cern.ch/cmsset_default.sh'
 
     def test_01_cvmfs_probe(self):
-        core.skip_ok_unless_installed('cvmfs', 'cvmfs-keys')
+        default_local = '/etc/cvmfs/default.local'
+        probe_repos = ",".join([
+            'atlas.cern.ch',
+            'cms.cern.ch',
+            'oasis.opensciencegrid.org'])
+        # Test depends on oasis-config to access the oasis.opensciencegrid.org
+        # repo. This is an external service, so the requirement should be
+        # removed as part of SOFTWARE-1108.
+        core.skip_ok_unless_installed('cvmfs', 'cvmfs-keys', 'oasis-config')
 
-        command = ('cat','/etc/cvmfs/default.local')
+
+        command = ('cat', default_local)
         status, stdout, stderr = core.system(command, False)
 
         if core.state['cvmfs.version'] < ('2', '1'):
             command = ('service','cvmfs', 'probe')
             status, stdout, stderr = core.system(command, False)
         else:
-            self.skip_ok('TODO: This test not implemented yet for CVMFS 2.1.X')
+            # Dave Dykstra suggested running cvmfs probe against a different
+            # set of repositories than are currently set up, so we modify them
+            # just for this test. (See SOFTWARE-1097)
+
+            # In the future, this test might be removed since we do not want
+            # to depend on external services, and it's redundant to probe the
+            # repos that we have already mounted.
+            files.replace(
+                default_local,
+                'CVMFS_REPOSITORIES=cms.cern.ch',
+                'CVMFS_REPOSITORIES=' + probe_repos,
+                owner='cvmfsprobe')
+            try:
+                command = ('cvmfs_config', 'probe')
+                status, stdout, stderr = core.system(command, False)
+                self.assertEqual(status, 0, core.diagnose('cvmfs probe', status, stdout, stderr))
+            finally:
+                files.restore(default_local, 'cvmfsprobe')
 
     def test_02_cvmfs(self):
         core.skip_ok_unless_installed('cvmfs', 'cvmfs-keys')
