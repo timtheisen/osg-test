73207469576f0426f99cd47a14ec8f45172761e1
Matyas Selmeci <matyas@cs.wisc.edu>
2013-07-23 21:55:09 +0000

    Refactor to reduce duplication
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-test/trunk@17230 4e558342-562e-0410-864c-e07659590f8c

--- osgtest/tests/test_52_bestman.py
+++ osgtest/tests/test_52_bestman.py
@@ -16,22 +16,27 @@ class TestBestman(osgunittest.OSGTestCase):
     __remote_path = __temp_dir + '/bestman_put_copied_file.txt'
     __local_path = __temp_dir + '/bestman_get_copied_file.txt'
 
-    def test_01_ping(self):
+
+    def setUp(self):
         core.skip_ok_unless_installed('bestman2-server', 'bestman2-client', 'voms-clients')
         self.skip_bad_unless(core.state['bestman.server-running'], 'bestman server not running')
-        srm_url = 'srm://%s:' % (TestBestman.__hostname)
-        command = ('srm-ping', srm_url + TestBestman.__port + '/' + TestBestman.__sfn )
+
+    def get_srm_url_base(self):
+        return 'srm://%s:%s/%s?SFN=' % (TestBestman.__hostname, TestBestman.__port, TestBestman.__sfn)
+
+    def get_srm_url(self):
+        return self.get_srm_url_base() + TestBestman.__remote_path
+
+    def test_01_ping(self):
+        command = ('srm-ping', self.get_srm_url_base())
         status, stdout, stderr = core.system(command, True)
 	fail = core.diagnose('Bestman Ping',
                              status, stdout, stderr)
         self.assertEqual(status, 0, fail) 
  
     def test_02_copy_local_to_server(self):
-        core.skip_ok_unless_installed('bestman2-server', 'bestman2-client', 'voms-clients')
-        self.skip_bad_unless(core.state['bestman.server-running'], 'bestman server not running')
         os.chmod(TestBestman.__temp_dir, 0777)
-        srm_url = 'srm://%s:%s/%s?SFN=%s' % (TestBestman.__hostname, TestBestman.__port, TestBestman.__sfn, TestBestman.__remote_path)
-        command = ('srm-copy', 'file:///' + TestBestman.__data_path,srm_url)
+        command = ('srm-copy', 'file:///' + TestBestman.__data_path, self.get_srm_url())
         status, stdout, stderr = core.system(command, True)
         fail = core.diagnose('Bestman copy, local to URL',
                              status, stdout, stderr)
@@ -40,10 +45,7 @@ class TestBestman(osgunittest.OSGTestCase):
         self.assert_(file_copied, 'Copied file missing')
 
     def test_03_copy_server_to_local(self):
-        core.skip_ok_unless_installed('bestman2-server', 'bestman2-client', 'voms-clients')
-        self.skip_bad_unless(core.state['bestman.server-running'], 'bestman server not running')
-	srm_url = 'srm://%s:%s/%s?SFN=%s' % (TestBestman.__hostname, TestBestman.__port, TestBestman.__sfn, TestBestman.__remote_path)
-	command = ('srm-copy', srm_url, 'file:///' + TestBestman.__local_path)
+	command = ('srm-copy', self.get_srm_url(), 'file:///' + TestBestman.__local_path)
         status, stdout, stderr = core.system(command, True)
         fail = core.diagnose('Bestman copy, URL to local',
                              status, stdout, stderr)
@@ -53,10 +55,7 @@ class TestBestman(osgunittest.OSGTestCase):
 	files.remove(TestBestman.__local_path)
 
     def test_04_remove_server_file(self):
-        core.skip_ok_unless_installed('bestman2-server', 'bestman2-client', 'voms-clients')
-        self.skip_bad_unless(core.state['bestman.server-running'], 'bestman server not running')
-        srm_url = 'srm://%s:%s/%s?SFN=%s' % (TestBestman.__hostname, TestBestman.__port, TestBestman.__sfn, TestBestman.__remote_path)
-        command = ('srm-rm', srm_url)
+        command = ('srm-rm', self.get_srm_url())
         status, stdout, stderr = core.system(command, True)
         fail = core.diagnose('Bestman remove, URL file',
                              status, stdout, stderr)
