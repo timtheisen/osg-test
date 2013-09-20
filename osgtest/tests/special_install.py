ca32a3edbbba6e389a980572b58360ad4980f6af
Brian Lin <blin@cs.wisc.edu>
2013-09-20 18:23:35 +0000

    Add Java7 specific installation logic
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-test/trunk@17757 4e558342-562e-0410-864c-e07659590f8c

--- osgtest/tests/special_install.py
+++ osgtest/tests/special_install.py
@@ -58,3 +58,23 @@ class TestInstall(osgunittest.OSGTestCase):
                 core.state['install.installed'].append(install_matches.group(1))
             elif update_matches is not None:
                 core.state['install.updated'].append(update_matches.group(1))
+
+    def test_05_fix_java_symlinks(self):
+        # This implements Section 5.1.2 of
+        # https://twiki.opensciencegrid.org/bin/view/Documentation/Release3/InstallSoftwareWithOpenJDK7
+        java7 = 'java-1.7.0-openjdk'
+        java7_devel = 'java-1.7.0-openjdk-devel'
+
+        jdk_installed = False
+        command = ('rpm', '--query', 'jdk')
+        _, stdout, _ = core.system(command)
+        if re.search("^jdk-1\.6\.\d+_\d+.*", stdout):
+            jdk_installed = True
+
+        if (jdk_installed and core.rpm_is_installed(java7) and core.rpm_is_installed(java7_devel)):
+            command = ('rm', '-f', '/usr/bin/java', '/usr/bin/javac', '/usr/bin/javadoc', '/usr/bin/jar')
+            core.check_system(command, 'Remove old symlinks')
+
+            command = ('yum', 'reinstall', '-y', java7, java7_devel)
+            core.check_system(command, 'Reinstall java7')
+        
