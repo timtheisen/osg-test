380448ba8a8096dfe7dfd5b90b06700655ba879c
Brian Lin <blin@cs.wisc.edu>
2013-10-11 16:48:07 +0000

    Preserve and restore pre-existing MySQL instance (SOFTWARE-1215)
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-test/trunk@17911 4e558342-562e-0410-864c-e07659590f8c

--- osgtest/tests/test_05_mysqld.py
+++ osgtest/tests/test_05_mysqld.py
@@ -1,13 +1,49 @@
 import os
+import re
+import shutil
 import unittest
 
 import osgtest.library.core as core
+import osgtest.library.files as files
 import osgtest.library.service as service
 import osgtest.library.osgunittest as osgunittest
 
 class TestStartMySQL(osgunittest.OSGTestCase):
 
-    def test_01_start_mysqld(self):
+    pidfile = '/var/run/mysqld/mysqld.pid'
+
+    def test_01_backup_mysql(self):
         core.skip_ok_unless_installed('mysql-server')
-        service.start('mysqld', sentinel_file='/var/run/mysqld/mysqld.pid')
+
+        # If the service is already running, we should stop it
+        if os.path.exists(pidfile):
+            service.stop('mysqld', sentinel_file=pidfile)
+
+        # Find the folder where the mysql files are stored
+        mysql_cfg = files.read('/etc/my.cnf')
+        for line in mysql_cfg:
+            try:
+                core.config['mysql.datadir'] = re.match('datadir=(.*)$', line.strip()).group(1)
+            except AttributeError, e:
+                if e.args[0] == "'NoneType' object has no attribute 'group'":
+                    # No match was found, move onto the next line
+                    continue
+                else:
+                    raise
+            else:
+                break
+        
+        # Backup the old mysql folder
+        backup = core.config['mysql.datadir'] + '-backup'
+        try:
+            shutil.move(core.config['mysql.datadir'], backup)
+        except OSError:
+            # Folder doesn't exist so we don't have to worry about backups
+            pass
+        else:
+            core.config['mysql.backup'] = backup
+
+    def test_02_start_mysqld(self):
+        core.skip_ok_unless_installed('mysql-server')
+        service.start('mysqld', sentinel_file=pidfile)
 
--- osgtest/tests/test_95_mysqld.py
+++ osgtest/tests/test_95_mysqld.py
@@ -2,6 +2,7 @@ import os
 import unittest
 
 import osgtest.library.core as core
+import osgtest.library.files as files
 import osgtest.library.service as service
 import osgtest.library.osgunittest as osgunittest
 
@@ -11,3 +12,13 @@ class TestStopMySQL(osgunittest.OSGTestCase):
         core.skip_ok_unless_installed('mysql-server')
         service.stop('mysqld')
 
+    def test_02_restore_backup(self):
+        core.skip_ok_unless_installed('mysql-server')
+
+        files.remove(core.config['mysql.datadir'], force=True)
+
+        try:
+            shutil.move(backup, core.config['mysql.backup'])
+        except KeyError:
+            # Backup was never created
+            pass
