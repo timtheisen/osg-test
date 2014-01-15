8c8b4bcda1d52afa7f42c96a1d3d479462943fd7
Tim Cartwright <cat@cs.wisc.edu>
2014-01-15 19:56:24 +0000

    Added retries to "yum install" commands, up to a given timeout (for now, 30 minutes) and only in cases when we believe that the install failure may be transient.  To start with, there is one "retry-able" failure mode supported, but it is easy to add more.
    Also renumbered a couple of functions, because the "04" sequence number
    was used twice.
    
    
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-test/trunk@18593 4e558342-562e-0410-864c-e07659590f8c

--- osgtest/tests/special_install.py
+++ osgtest/tests/special_install.py
@@ -1,8 +1,11 @@
+import re
+import time
+
 import osgtest.library.core as core
 import osgtest.library.osgunittest as osgunittest
-import re
 
 class TestInstall(osgunittest.OSGTestCase):
+
     def clean_yum(self):
         pre = ('yum', '--enablerepo=*', 'clean')
         core.check_system(pre + ('all',), 'YUM clean all')
@@ -20,7 +23,13 @@ class TestInstall(osgunittest.OSGTestCase):
                 core.state['install.installed'].append(install_matches.group(1))
             if update_matches is not None:
                 core.state['install.updated'].append(update_matches.group(1))
-                
+
+    def install_failure_can_be_retried(output):
+        """Scan 'yum install' output to see if a retry might succeed."""
+        if re.search(r'Error Downloading Packages:\n.*No more mirrors to try', output):
+            return True
+        return False
+
     def test_01_yum_repositories(self):
         pre = ('rpm', '--verify', '--nomd5', '--nosize', '--nomtime')
         core.check_system(pre + ('epel-release',), 'Verify epel-release')
@@ -32,7 +41,7 @@ class TestInstall(osgunittest.OSGTestCase):
 
     def test_02_clean_yum(self):
         self.clean_yum()
-        
+
     def test_03_install_packages(self):
         core.state['install.success'] = False
 
@@ -42,30 +51,54 @@ class TestInstall(osgunittest.OSGTestCase):
         # Install packages
         core.state['install.transaction_ids'] = []
         fail_msg = ''
+        deadline = time.time() + 1800   # 30 minutes from now
         for package in core.options.packages:
+
+            # Do not try to re-install packages
             if core.rpm_is_installed(package):
                 continue
-            command = ['yum', '-y']
-            for repo in core.options.extrarepos:
-                command.append('--enablerepo=%s' % repo)
-            command += ['install', package]
-            status, stdout, stderr = core.system(command)
 
-            if status != 0:
-                fail_msg = fail_msg + core.diagnose('Installation of %s failed' % package, status, stdout, stderr)
-            else:
-                # RHEL6 doesn't have the rollback option so we have to store the transaction id's so
-                # we can undo each transaction in the proper order
-                if el_version == 6:
-                    command = ('yum', 'history', 'info')
-                    history_out = core.check_system(command, 'Get yum Transaction ID')[0]
-                    m = re.search('Transaction ID : (\d*)', history_out)
-                    core.state['install.transaction_ids'].append(m.group(1))
+            # Loop for retries
+            while True:
 
-                command = ('rpm', '--verify', package)
-                core.check_system(command, 'Verify %s' % (package))
+                # Stop (re)trying if the deadline has passed
+                if time.time() > deadline:
+                    fail_msg += 'Installation attempt(s) terminated after timeout period'
+                    break
 
-                self.parse_install_output(stdout.strip().split('\n'))
+                # Attempt installation
+                command = ['yum', '-y']
+                for repo in core.options.extrarepos:
+                    command.append('--enablerepo=%s' % repo)
+                command += ['install', package]
+                status, stdout, stderr = core.system(command)
+
+                # Deal with success
+                if status == 0:
+                    # RHEL 6 does not have the rollback option, so store the
+                    # transaction IDs so we can undo each transaction in the
+                    # proper order
+                    if el_version == 6:
+                        command = ('yum', 'history', 'info')
+                        history_out = core.check_system(command, 'Get yum Transaction ID')[0]
+                        m = re.search('Transaction ID : (\d*)', history_out)
+                        core.state['install.transaction_ids'].append(m.group(1))
+                    command = ('rpm', '--verify', package)
+                    core.check_system(command, 'Verify %s' % (package))
+                    self.parse_install_output(stdout.strip().split('\n'))
+                    break
+
+                # Deal with failures that can be retried
+                elif install_failure_can_be_retried(stdout):
+                    time.sleep(30)
+                    core.log_message('Retrying install of %s' % (package))
+                    continue
+
+                # Otherwise, we do not expect a retry to succeed, ever, so fail
+                # this package
+                else:
+                    fail_msg = fail_msg + core.diagnose('Installation of %s failed' % package, status, stdout, stderr)
+                    break
                 
         if fail_msg:
             self.fail(fail_msg)
@@ -91,7 +124,7 @@ class TestInstall(osgunittest.OSGTestCase):
         if not core.options.updaterepo:
             core.options.updaterepo = 'osg'
         
-    def test_04_update_packages(self):
+    def test_05_update_packages(self):
         if not (core.options.updaterepo and core.state['install.installed']):
             return
         
@@ -106,7 +139,7 @@ class TestInstall(osgunittest.OSGTestCase):
         # Parse output for order of installs
         self.parse_install_output(stdout.strip().split('\n'))
 
-    def test_05_fix_java_symlinks(self):
+    def test_06_fix_java_symlinks(self):
         # This implements Section 5.1.2 of
         # https://twiki.opensciencegrid.org/bin/view/Documentation/Release3/InstallSoftwareWithOpenJDK7
         java7 = 'java-1.7.0-openjdk'
