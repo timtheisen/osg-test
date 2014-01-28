362b993ee3312b4eb34fe2f3558bc1828bf7fca5
Brian Lin <blin@cs.wisc.edu>
2014-01-28 19:58:38 +0000

    Improve readability of formatted commands by using double quotes instead of single quotes
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-test/trunk@18691 4e558342-562e-0410-864c-e07659590f8c

--- osgtest/library/core.py
+++ osgtest/library/core.py
@@ -351,7 +351,7 @@ def __format_command(command):
         if part == '':
             result.append("''")
         elif re.search(r"[' \\]", part):
-            result.append("'" + part + "'")
+            result.append('"' + part + '"')
         else:
             result.append(part)
     return result
