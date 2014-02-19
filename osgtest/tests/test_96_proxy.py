4c75899c51a750cec12391e015f2fc9e72a0aef8
Brian Lin <blin@cs.wisc.edu>
2014-02-19 22:26:21 +0000

    Reapply r18784, replace su with runuser
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-test/trunk@18797 4e558342-562e-0410-864c-e07659590f8c

---
+++ osgtest/tests/test_96_proxy.py
@@ -0,0 +1,9 @@
+import osgtest.library.core as core
+import osgtest.library.osgunittest as osgunittest
+
+class TestGridProxyDestroy(osgunittest.OSGTestCase):
+
+    def test_01_check_proxy(self):
+        core.skip_ok_unless_installed('globus-proxy-utils')
+        command = ('grid-proxy-destroy', '-debug')
+        core.check_system(command, 'Run grid-proxy-destroy', user=True)
