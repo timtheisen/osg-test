e11179cbc003b283fd40dc5e1462304d0b61e97c
Brian Lin <blin@cs.wisc.edu>
2014-02-20 19:43:13 +0000

    Fix formatting in log and add skips to proxy tests
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-test/trunk@18809 4e558342-562e-0410-864c-e07659590f8c

--- osgtest/tests/test_40_proxy.py
+++ osgtest/tests/test_40_proxy.py
@@ -11,6 +11,7 @@ class TestGridProxyInit(osgunittest.OSGTestCase):
         core.log_message(stdout)
 
     def test_01_grid_proxy_init(self):
+        core.state['proxy.created'] = False
         core.skip_ok_unless_installed('globus-proxy-utils')
         command = ('grid-proxy-init', '-debug')
         password = core.options.password + '\n'
@@ -21,10 +22,12 @@ class TestGridProxyInit(osgunittest.OSGTestCase):
             attributes = {}
             for x in dir(e):
                 attributes[x] = getattr(e,x, None)
-            pprint.pprint(attributes)
+            core.log_message(pprint.pformat(attributes))
             raise
+        core.state['proxy.created'] = True
 
     def test_02_grid_proxy_info(self):
         core.skip_ok_unless_installed('globus-proxy-utils')
+        self.skip_bad_unless(core.state['proxy.created'], 'missing proxy')
         command = ('grid-proxy-info', '-debug')
         core.check_system(command, 'Run grid-proxy-info', user=True)
