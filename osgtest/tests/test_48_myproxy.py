1cddcff875260d34030167878f21026fa2736fda
Edgar Fajardo <efajardo@physics.ucsd.edu>
2014-04-04 18:49:13 +0000

    Fix to myproxy init and retrieval to include the username under which the proxy should be stored
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-test/trunk@19071 4e558342-562e-0410-864c-e07659590f8c

--- osgtest/tests/test_48_myproxy.py
+++ osgtest/tests/test_48_myproxy.py
@@ -38,7 +38,7 @@ class TestMyProxy(osgunittest.OSGTestCase):
         core.skip_ok_unless_installed('myproxy', 'myproxy-server')
         proxypath = core.state['proxy.path']
         # The -S option is given in the command so it accepts the stdin input for the passowrds
-        command = ('myproxy-init','--verbose','-C', proxypath, '-y', proxypath, '-s', 'localhost' ,'-S')      
+        command = ('myproxy-init','--verbose','-C', proxypath, '-y', proxypath, '-s', 'localhost' , '-S', '-l', core.options.username)      
         # We give an already created proxy to my proxy and password to store it
         password = core.config['myproxy.password']
         core.check_system(command, 'Normal myproxy-init', user=True, stdin=password)
@@ -47,6 +47,6 @@ class TestMyProxy(osgunittest.OSGTestCase):
     def test_05_my_proxy_retrieval(self):
         core.skip_ok_unless_installed('myproxy', 'myproxy-server')
         self.skip_bad_unless(core.state['myproxy.created'], 'MyProxy creation failed')
-        command = ('myproxy-logon', '--verbose', '-s', 'localhost')
+        command = ('myproxy-logon', '--verbose', '-s', 'localhost', '-l', core.options.username)
         password = core.config['myproxy.password'] + '\n'
         core.check_system(command, 'myproxy-logon retrieval', user=True, stdin=password)
