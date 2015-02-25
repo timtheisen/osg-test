3a131ed363f5ba9df7e70a2718bf348bf2e42f9a
Brian Lin <blin@cs.wisc.edu>
2015-02-25 17:34:38 +0000

    Add gfal2-plugin-file requirement (SOFTWARE-1799)
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-test/trunk@20690 4e558342-562e-0410-864c-e07659590f8c

--- osgtest/tests/test_58_gfal2util.py
+++ osgtest/tests/test_58_gfal2util.py
@@ -15,7 +15,7 @@ class TestGFAL2Util(osgunittest.OSGTestCase):
 
 
     def setUp(self):
-        core.skip_ok_unless_installed('bestman2-server', 'gfal2-util', 'gfal2-plugin-srm', 'voms-clients')
+        core.skip_ok_unless_installed('bestman2-server', 'gfal2-util', 'gfal2-plugin-srm', 'gfal2-plugin-file', 'voms-clients')
         self.skip_bad_unless(core.state['bestman.server-running'], 'bestman server not running')
 
     def get_srm_url_base(self):
