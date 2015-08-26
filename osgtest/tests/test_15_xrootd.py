697e227d97c0e5d917574efdf7adfa407af0f075
Matyas Selmeci <matyas@cs.wisc.edu>
2015-08-26 20:16:31 +0000

    Fix up style in xrootd setup
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-test/trunk@21577 4e558342-562e-0410-864c-e07659590f8c

--- osgtest/tests/test_15_xrootd.py
+++ osgtest/tests/test_15_xrootd.py
@@ -7,22 +7,34 @@ import osgtest.library.certificates as certs
 import unittest
 import re
 
+XROOTD_CFG_TEXT = """\
+cms.space min 2g 5g
+xrootd.seclib /usr/lib64/libXrdSec.so
+sec.protocol /usr/lib64 gsi -certdir:/etc/grid-security/certificates -cert:/etc/grid-security/xrd/xrdcert.pem -key:/etc/grid-security/xrd/xrdkey.pem -crl:3 -gridmap:/etc/grid-security/xrd/xrdmapfile --gmapopt:10 --gmapto:0
+acc.authdb /etc/xrootd/auth_file
+ofs.authorize
+"""
+
+AUTHFILE_TEXT = """\
+u * /tmp a
+u = /tmp/@=/ a
+u xrootd /tmp a
+"""
+
 class TestStartXrootd(osgunittest.OSGTestCase):
 
     def test_01_start_xrootd(self):
         core.config['xrootd.pid-file']='/var/run/xrootd/xrootd-default.pid'
         core.config['certs.hostcert'] = '/etc/grid-security/hostcert.pem'
         core.config['certs.hostkey'] = '/etc/grid-security/hostkey.pem'
-        core.config['certs.xrootdcert']='/etc/grid-security/xrd/xrdcert.pem'
-        core.config['certs.xrootdkey']='/etc/grid-security/xrd/xrdkey.pem'
-        core.config['xrootd.gsi']="ON"
+        core.config['certs.xrootdcert'] = '/etc/grid-security/xrd/xrdcert.pem'
+        core.config['certs.xrootdkey'] = '/etc/grid-security/xrd/xrdkey.pem'
+        core.config['xrootd.gsi'] = "ON"
         core.state['xrootd.started-server'] = False
         core.state['xrootd.backups-exist'] = False
         
         vdt_pw = pwd.getpwnam(core.options.username)
-        core.config['certs.usercert'] = os.path.join(vdt_pw.pw_dir,
-                                                     '.globus',
-                                                     'usercert.pem')
+        core.config['certs.usercert'] = os.path.join(vdt_pw.pw_dir, '.globus', 'usercert.pem')
         core.skip_ok_unless_installed('xrootd', by_dependency=True)
                   
         # Determine xrootd package name
@@ -34,21 +46,13 @@ class TestStartXrootd(osgunittest.OSGTestCase):
         user = pwd.getpwnam("xrootd")
         if core.config['xrootd.gsi'] == "ON":
             core.skip_ok_unless_installed('globus-proxy-utils')
-            certs.install_cert('certs.xrootdcert', 'certs.hostcert', 
-                'xrootd', 0644)
-            certs.install_cert('certs.xrootdkey', 'certs.hostkey', 
-                'xrootd', 0400)
+            certs.install_cert('certs.xrootdcert', 'certs.hostcert', 'xrootd', 0644)
+            certs.install_cert('certs.xrootdkey', 'certs.hostkey', 'xrootd', 0400)
 
-            cfgfile='/etc/xrootd/xrootd-clustered.cfg'
-            cfgtext='cms.space min 2g 5g\n'
-            cfgtext=cfgtext+'xrootd.seclib /usr/lib64/libXrdSec.so\n'
-            cfgtext=cfgtext+'sec.protocol /usr/lib64 gsi -certdir:/etc/grid-security/certificates -cert:/etc/grid-security/xrd/xrdcert.pem -key:/etc/grid-security/xrd/xrdkey.pem -crl:3 -gridmap:/etc/grid-security/xrd/xrdmapfile --gmapopt:10 --gmapto:0\n'
-            cfgtext=cfgtext+'acc.authdb /etc/xrootd/auth_file\n'
-            cfgtext=cfgtext+'ofs.authorize\n'
-            files.append(cfgfile,cfgtext,owner='xrootd',backup=True)
-            authfile='/etc/xrootd/auth_file'
-            files.write(authfile,'u * /tmp a\nu = /tmp/@=/ a\nu xrootd /tmp a\n', owner="xrootd",
-                        chown=(user.pw_uid, user.pw_gid))
+            cfgfile = '/etc/xrootd/xrootd-clustered.cfg'
+            files.append(cfgfile, XROOTD_CFG_TEXT, owner='xrootd', backup=True)
+            authfile = '/etc/xrootd/auth_file'
+            files.write(authfile, AUTHFILE_TEXT, owner="xrootd", chown=(user.pw_uid, user.pw_gid))
             
             user_dn = certs.certificate_info(core.config['certs.usercert'])[1]
             files.write("/etc/grid-security/xrd/xrdmapfile","\"%s\" vdttest" % user_dn, owner="xrootd",
@@ -61,7 +65,6 @@ class TestStartXrootd(osgunittest.OSGTestCase):
         self.assertEqual(status, 0, 'Start Xrootd server exited %d' % status)
 
         self.assert_(stdout.find('FAILED') == -1, 'Start Xrootd server failed')
-        self.assert_(os.path.exists(core.config['xrootd.pid-file']),
-                     'xrootd server PID file missing')
+        self.assert_(os.path.exists(core.config['xrootd.pid-file']), 'xrootd server PID file missing')
         core.state['xrootd.started-server'] = True
 
