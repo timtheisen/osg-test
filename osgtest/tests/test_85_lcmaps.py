59945afd33c918b6aa095790263991aaccf86157
Brian Lin <blin@cs.wisc.edu>
2015-10-23 15:51:07 +0000

    Standardize lcmaps cleanup class name
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-test/trunk@21921 4e558342-562e-0410-864c-e07659590f8c

--- osgtest/tests/test_85_lcmaps.py
+++ osgtest/tests/test_85_lcmaps.py
@@ -4,7 +4,7 @@ import osgtest.library.core as core
 import osgtest.library.files as files
 import osgtest.library.osgunittest as osgunittest
 
-class RestoreLcMaps(osgunittest.OSGTestCase):
+class TestRestoreLcMaps(osgunittest.OSGTestCase):
 
     def test_01_restore_lcmaps_after_glexec(self):
         core.skip_ok_unless_installed('glexec')
