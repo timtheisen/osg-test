ab4b2f048a1d747102c8cecfeb0c7ae580a4b7ad
Matyas Selmeci <matyas@cs.wisc.edu>
2016-09-20 14:29:08 -0500

    Refactor: use service.is_running() for checking if condor-cron is running or not

--- osgtest/tests/test_11_condor_cron.py
+++ osgtest/tests/test_11_condor_cron.py
@@ -1,26 +1,16 @@
 import os
 import osgtest.library.core as core
 import osgtest.library.osgunittest as osgunittest
+import osgtest.library.service as service
 import unittest
 
 class TestStartCondorCron(osgunittest.OSGTestCase):
-    def is_running(self):
-        # TODO Move this to library/service.py
-        if core.el_release() < 7:
-            command = ['service', 'condor-cron', 'status']
-        else:
-            command = ['systemctl', 'is-active', 'condor-cron']
-
-        status, _, _ = core.system(command)
-
-        return status == 0
-
     def test_01_start_condor_cron(self):
         core.state['condor-cron.started-service'] = False
         core.state['condor-cron.running-service'] = False
 
         core.skip_ok_unless_installed('condor-cron')
-        if self.is_running():
+        if service.is_running('condor-cron'):
             core.state['condor-cron.running-service'] = True
             self.skip_ok('already running')
 
@@ -31,7 +21,7 @@ class TestStartCondorCron(osgunittest.OSGTestCase):
         else:
             core.check_system(('systemctl', 'start', 'condor-cron'), 'Start Condor-Cron')
 
-        self.assert_(self.is_running(), "Condor-Cron is not running")
+        self.assert_(service.is_running('condor-cron'), "Condor-Cron is not running")
 
         core.state['condor-cron.started-service'] = True
         core.state['condor-cron.running-service'] = True
--- osgtest/tests/test_88_condor_cron.py
+++ osgtest/tests/test_88_condor_cron.py
@@ -1,20 +1,10 @@
 import os
 import osgtest.library.core as core
 import osgtest.library.osgunittest as osgunittest
+import osgtest.library.service as service
 import unittest
 
 class TestStopCondorCron(osgunittest.OSGTestCase):
-    def is_running(self):
-        # TODO Move this to library/service.py
-        if core.el_release() < 7:
-            command = ['service', 'condor-cron', 'status']
-        else:
-            command = ['systemctl', 'is-active', 'condor-cron']
-
-        status, _, _ = core.system(command)
-
-        return status == 0
-
     def test_01_stop_condor_cron(self):
         core.skip_ok_unless_installed('condor-cron')
         self.skip_ok_if(core.state['condor-cron.started-service'] == False, 'did not start server')
@@ -26,6 +16,6 @@ class TestStopCondorCron(osgunittest.OSGTestCase):
         else:
             core.check_system(('systemctl', 'stop', 'condor-cron'), 'Stop Condor-Cron')
 
-        self.assertFalse(self.is_running(), 'Condor-Cron still active')
+        self.assertFalse(service.is_running('condor-cron'), 'Condor-Cron still active')
 
         core.state['condor-cron.running-service'] = False
