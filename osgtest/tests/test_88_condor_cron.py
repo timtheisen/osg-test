6bb0e97d42c6e3f789dab28e22de02f61dca9d76
Brian Lin <brianhlin@gmail.com>
2016-09-23 14:52:52 -0500

    Update condor-cron to use systemd and timeout supported service functions

--- osgtest/tests/test_88_condor_cron.py
+++ osgtest/tests/test_88_condor_cron.py
@@ -1,21 +1,13 @@
-import os
 import osgtest.library.core as core
 import osgtest.library.osgunittest as osgunittest
 import osgtest.library.service as service
-import unittest
 
 class TestStopCondorCron(osgunittest.OSGTestCase):
     def test_01_stop_condor_cron(self):
         core.skip_ok_unless_installed('condor-cron')
         self.skip_ok_if(core.state['condor-cron.started-service'] == False, 'did not start server')
 
-        if core.el_release() < 7:
-            command = ('service', 'condor-cron', 'stop')
-            stdout, _, fail = core.check_system(command, 'Stop Condor-Cron')
-            self.assert_(stdout.find('error') == -1, fail)
-        else:
-            core.check_system(('systemctl', 'stop', 'condor-cron'), 'Stop Condor-Cron')
-
+        service.stop('condor-cron')
         self.assertFalse(service.is_running('condor-cron'), 'Condor-Cron still active')
 
         core.state['condor-cron.running-service'] = False
