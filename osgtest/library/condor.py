45ce5320ab1b78bf0bf604059736d0aec3cac418
Brian Lin <brianhlin@gmail.com>
2016-10-21 16:03:24 -0500

    Add convenience functions for getting condor config values

--- osgtest/library/condor.py
+++ osgtest/library/condor.py
@@ -19,7 +19,6 @@ def lockfile_path():
             condor_lockfile = '/var/lock/subsys/condor'
     return condor_lockfile
 
-
 def wait_for_daemon(collector_log_path, stat, daemon, timeout):
     """Wait until the requested 'daemon' is available and accepting commands by
     monitoring the specified CollectorLog from the position specified by 'stat'
@@ -30,3 +29,31 @@ def wait_for_daemon(collector_log_path, stat, daemon, timeout):
     sentinel = r'%sAd\s+:\s+Inserting' % daemon.capitalize()
     return bool(core.monitor_file(collector_log_path, stat, sentinel, timeout)[0])
 
+def config_val(attr):
+    """Query HTCondor for the value of a configuration variable using the python
+    bindings if available, condor_config_val otherwise
+
+    """
+    try:
+        import htcondor
+        # Necessary for checking config between different flavors of HTCondor
+        htcondor.reload_config()
+        try:
+            val = htcondor.param[attr]
+        except KeyError: # attr is undefined
+            val = None
+    except:
+        out, _, _ = core.check_system(('condor_config_val', attr),
+                                      'Failed to query for config variable: %s' % attr)
+        val = out.strip()
+    return val
+
+def ce_config_val(attr):
+    """Query HTCondor-CE for the value of a configuration variable using the
+    python bindings if available, condor_config_val otherwise
+
+    """
+    os.environ.setdefault('CONDOR_CONFIG', '/etc/condor-ce/condor_config')
+    val = config_val(attr)
+    del os.environ['CONDOR_CONFIG']
+    return val
