dd9f59cc7247d0e4076694ebb744f7d9b059831a
Matyas Selmeci <matyas@cs.wisc.edu>
2016-11-02 10:15:25 -0500

    Use service library to start/stop gridftp

--- osgtest/tests/test_86_gridftp.py
+++ osgtest/tests/test_86_gridftp.py
@@ -1,16 +1,12 @@
 import os
 import osgtest.library.core as core
 import osgtest.library.osgunittest as osgunittest
-import unittest
+import osgtest.library.service as service
 
 class TestStopGridFTP(osgunittest.OSGTestCase):
 
     def test_01_stop_gridftp(self):
         core.skip_ok_unless_installed('globus-gridftp-server-progs')
         self.skip_ok_if(core.state['gridftp.started-server'] == False, 'did not start server')
-
-        command = ('service', 'globus-gridftp-server', 'stop')
-        stdout, _, fail = core.check_system(command, 'Stop GridFTP server')
-        self.assert_(stdout.find('FAILED') == -1, fail)
-        self.assert_(not os.path.exists(core.config['gridftp.pid-file']),
-                     'GridFTP server PID file still present')
+        service.check_stop('globus-gridftp-server')
+        core.state['gridftp.running-server'] = False
