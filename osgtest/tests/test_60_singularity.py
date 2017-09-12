f53790a610eea70497228edfd8f9f333be80d1d0
efajardo <efajardo@physics.ucsd.edu>
2017-09-12 09:56:02 -0700

    Changed the command to test

--- osgtest/tests/test_60_singularity.py
+++ osgtest/tests/test_60_singularity.py
@@ -33,7 +33,7 @@ class TestSingularity(osgunittest.OSGTestCase):
         self.assert_(file_exists, 'cvfms image missing')
         
         #command = ('bash', '-c', 'source ' + self.__check_path)
-        command= ('singularity', 'exec', '--bind', '/cvmfs', self.__cvmfs_image, 'lsb_release -a')
+        command= ('singularity', 'exec', '--bind', '/cvmfs', self.__cvmfs_image)
         status, stdout, stderr = core.system(command, False)
         fail = core.diagnose('singularity checking a file',
                              command, status, stdout, stderr)
