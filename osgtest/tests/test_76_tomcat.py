e12a5dab71457c2b8b244af30a3a8656d481949d
Matyas Selmeci <matyas@cs.wisc.edu>
2017-09-20 18:11:25 -0500

    If haveged available, use it to generate entropy for tomcat (SOFTWARE-2880)

--- osgtest/tests/test_76_tomcat.py
+++ osgtest/tests/test_76_tomcat.py
@@ -64,3 +64,6 @@ class TestStopTomcat(osgunittest.OSGTestCase):
 
         files.restore(tomcat.conffile(), 'tomcat')
 
+    def test_08_stop_haveged(self):
+        core.skip_ok_unless_installed(tomcat.pkgname(), 'haveged')
+        service.check_stop('haveged')
