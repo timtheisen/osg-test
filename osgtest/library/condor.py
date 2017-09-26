00edb389b775a4ec692cd16e494bee6c7a6d3b22
Matyas Selmeci <matyas@cs.wisc.edu>
2017-09-26 18:00:30 -0500

    Remove condor.lockfile_path(), unused

--- osgtest/library/condor.py
+++ osgtest/library/condor.py
@@ -2,23 +2,6 @@ import os
 
 import osgtest.library.core as core
 
-def lockfile_path():
-    """The path to the condor lockfile (EL5 and EL6 only)
-    Returns None on EL7.
-
-    """
-    if core.el_release() >= 7:
-        return None
-
-    condor_lockfile = '/var/lock/subsys/condor_master'
-    # The name of the lockfile changed in 7.8.8
-    if core.rpm_is_installed('condor'):
-        condor_version = core.get_package_envra('condor')[2]
-        condor_version_split = condor_version.split('.')
-        if condor_version_split >= ['7', '8', '8']:
-            condor_lockfile = '/var/lock/subsys/condor'
-    return condor_lockfile
-
 def wait_for_daemon(collector_log_path, stat, daemon, timeout):
     """Wait until the requested 'daemon' is available and accepting commands by
     monitoring the specified CollectorLog from the position specified by 'stat'
