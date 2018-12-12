a8f663e4b500b9847e1258f11112abd4232ff6d8
efajardo <efajardo@physics.ucsd.edu>
2018-12-12 14:21:54 -0800

    Starting the privlieged service

--- osgtest/tests/test_150_xrootd.py
+++ osgtest/tests/test_150_xrootd.py
@@ -33,6 +33,7 @@ class TestStartXrootd(osgunittest.OSGTestCase):
         core.config['certs.xrootdkey'] = '/etc/grid-security/xrd/xrdkey.pem'
         core.config['xrootd.config'] = '/etc/xrootd/xrootd-clustered.cfg'
         core.config['xrootd.gsi'] = "ON"
+        core.config['xrootd.multiuser'] = "OFF"
         core.state['xrootd.started-server'] = False
         core.state['xrootd.backups-exist'] = False
 
@@ -81,6 +82,8 @@ class TestStartXrootd(osgunittest.OSGTestCase):
         core.skip_ok_unless_installed('xrootd', by_dependency=True)
         if core.el_release() < 7:
             core.config['xrootd_service'] = "xrootd"
+        elif core.config['xrootd.multiuser'] == "ON":
+            core.config['xrootd_service'] = "xrootd-privileged@clustered"
         else:
             core.config['xrootd_service'] = "xrootd@clustered"
 
