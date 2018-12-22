1ad85684c24ba4f8d3b6058e1b432984a1f65ae1
Matyas Selmeci <matyas@cs.wisc.edu>
2018-12-21 18:04:40 -0600

    OSG 3.3 purge: drop GUMS tests

--- osgtest/tests/test_750_gums.py
+++
@@ -1,9 +0,0 @@
-import osgtest.library.core as core
-import osgtest.library.files as files
-import osgtest.library.osgunittest as osgunittest
-
-class TestStopGUMS(osgunittest.OSGTestCase):
-
-    def test_01_restore_files(self):
-        core.skip_ok_unless_installed('gums-service')
-        files.restore(core.config['gums.config-file'], 'gums')
