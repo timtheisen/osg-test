b3702552ab83b7910b7972512253a829bbc56488
Matyas Selmeci <matyas@cs.wisc.edu>
2019-05-01 11:06:03 -0500

    Add xrootd and non-el6 check for xrootd-tpc cleanup too

--- osgtest/tests/test_838_xrootd_tpc.py
+++ osgtest/tests/test_838_xrootd_tpc.py
@@ -5,12 +5,16 @@ import osgtest.library.osgunittest as osgunittest
 
 class TestStopXrootdTPC(osgunittest.OSGTestCase):
     @core.elrelease(7,8)
+    def setUp(self):
+        core.skip_ok_unless_installed("xrootd",
+                                      by_dependency=True)
+
     def test_01_stop_xrootd(self):
         if core.state['xrootd.tpc.backups-exist']:
             files.restore(core.config['xrootd.tpc.config-1'], "xrootd")
             files.restore(core.config['xrootd.tpc.config-2'], "xrootd")
 
-        core.skip_ok_unless_installed('xrootd', 'xrootd-scitokens', by_dependency=True)
+        core.skip_ok_unless_installed('xrootd-scitokens', by_dependency=True)
         self.skip_ok_if(not core.state['xrootd.started-http-server-1'] and 
                         not core.state['xrootd.started-http-server-2'], 
                         'did not start any of the http servers')
