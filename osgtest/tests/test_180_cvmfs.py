893a91eea245f48a79a631c9b878597bea2fcea6
Brian Lin <brianhlin@gmail.com>
2020-10-08 11:53:59 -0500

    Pylint: move functions without self usage outside of the test class

--- osgtest/tests/test_180_cvmfs.py
+++ osgtest/tests/test_180_cvmfs.py
@@ -8,53 +8,56 @@ CVMFS_HTTP_PROXY="http://squid-cs-b240.chtc.wisc.edu:3128|http://squid-cs-2360.c
 """
 
 
+def setup_fuse():
+    fuse_conf_path = '/etc/fuse.conf'
+    files.preserve(fuse_conf_path, 'cvmfs')
+    try:
+        contents = files.read(fuse_conf_path)
+    except IOError:
+        # Sometimes this file doesn't exist
+        contents = []
+    for line in contents:
+        if "user_allow_other" in line:
+            return
+    contents.append("user_allow_other\n")
+    files.write(fuse_conf_path, contents, owner='cvmfs', backup=False, chmod=0o644)
+
+
+def setup_automount():
+    automount_conf_path = '/etc/auto.master'
+    files.preserve(automount_conf_path, 'cvmfs')
+    try:
+        contents = files.read(automount_conf_path)
+    except IOError:
+        # Sometimes this file doesn't exist
+        contents = []
+    for line in contents:
+        if "cvmfs" in line:
+            return
+    contents.append("/cvmfs /etc/auto.cvmfs\n")
+    files.write(automount_conf_path, contents, owner='cvmfs', backup=False, chmod=0o644)
+
+
+def setup_cvmfs():
+    command = ('mkdir', '-p', '/tmp/cvmfs')
+    core.system(command, False)
+    files.write("/etc/cvmfs/default.local", CVMFS_CONFIG, owner='cvmfs', chmod=0o644)
+
+    # Write verbose debug log for the OASIS repo
+    oasis_repo = "oasis.opensciencegrid.org"
+    files.write("/etc/cvmfs/config.d/%s.local" % oasis_repo,
+                "CVMFS_DEBUGLOG=/tmp/cvmfs/%s.log" % oasis_repo,
+                owner='cvmfs', chmod=0o644)
+
+
 class TestStartCvmfs(osgunittest.OSGTestCase):
-
-    def setup_fuse(self):
-        fuse_conf_path = '/etc/fuse.conf'
-        files.preserve(fuse_conf_path, 'cvmfs')
-        try:
-            contents = files.read(fuse_conf_path)
-        except IOError:
-            # Sometimes this file doesn't exist
-            contents = []
-        for line in contents:
-            if "user_allow_other" in line:
-                return
-        contents.append("user_allow_other\n")
-        files.write(fuse_conf_path, contents, owner='cvmfs', backup=False, chmod=0o644)
-
-    def setup_automount(self):
-        automount_conf_path = '/etc/auto.master'
-        files.preserve(automount_conf_path, 'cvmfs')
-        try:
-            contents = files.read(automount_conf_path)
-        except IOError:
-            # Sometimes this file doesn't exist
-            contents = []
-        for line in contents:
-            if "cvmfs" in line:
-                return
-        contents.append("/cvmfs /etc/auto.cvmfs\n")
-        files.write(automount_conf_path, contents, owner='cvmfs', backup=False, chmod=0o644)
-
-    def setup_cvmfs(self):
-        command = ('mkdir', '-p', '/tmp/cvmfs')
-        core.system(command, False)
-        files.write("/etc/cvmfs/default.local", CVMFS_CONFIG, owner='cvmfs', chmod=0o644)
-
-        # Write verbose debug log for the OASIS repo
-        oasis_repo = "oasis.opensciencegrid.org"
-        files.write("/etc/cvmfs/config.d/%s.local" % oasis_repo,
-                    "CVMFS_DEBUGLOG=/tmp/cvmfs/%s.log" % oasis_repo,
-                    owner='cvmfs', chmod=0o644)
 
     def test_01_setup_cvmfs(self):
         core.skip_ok_unless_installed('cvmfs')
 
-        self.setup_fuse()
-        self.setup_automount()
-        self.setup_cvmfs()
+        setup_fuse()
+        setup_automount()
+        setup_cvmfs()
 
     def test_02_start_cvmfs(self):
         core.state['cvmfs.started-server'] = False
