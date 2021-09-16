13ed435ca328fa81b796bbacfab8f53e30e52843
Matyas Selmeci <matyas@cs.wisc.edu>
2021-09-16 10:14:32 -0500

    Add "force" argument to service functions to ignore core.state flags and always start/stop the service

--- osgtest/library/service.py
+++ osgtest/library/service.py
@@ -7,7 +7,7 @@ from osgtest.library import files
 STATUS_RUNNING = 0 # LSB: program is running or service is OK
 STATUS_STOPPED = 3 # LSB: program is not running according to LSB init standards
 
-def start(service_name):
+def start(service_name, force=False):
     """
     Start a service via init script or systemd.
 
@@ -20,12 +20,14 @@ def start(service_name):
     The service is not started up if core.state[service_name.started-service] is
     True.
 
+    If `force` is True, ignore the above flag and always start the service.
+
     The following globals are set:
     core.config[service_name.sentinel-file] is set to the value of sentinel_file,
     if specified.
 
     """
-    if core.state.get(service_name + '.started-service'):
+    if not force and core.state.get(service_name + '.started-service'):
         core.skip('service ' + service_name + ' already running (flagged as started)')
         return
 
@@ -36,18 +38,18 @@ def start(service_name):
     core.check_system(command, 'Start ' + service_name + ' service')
     core.state[service_name + '.started-service'] = True
 
-def check_start(service_name, timeout=10, log_to_check = None, min_up_time=0):
+def check_start(service_name, timeout=10, log_to_check = None, min_up_time=0, force=False):
     """
     Start a service, 'service_name' via init script or systemd and ensure that
     it starts running within a 'timeout' second window (default=10s).
     Will wait 'min_up_time' seconds before checking; the timeout window starts
     after min_up_time has been reached.
     """
-    start(service_name)
+    start(service_name, force)
     time.sleep(min_up_time)
     assert is_running(service_name, timeout=timeout, log_to_check=log_to_check), "%s is not running" % service_name
 
-def stop(service_name):
+def stop(service_name, force=False):
     """
     Stop a service via init script or systemd.
 
@@ -61,8 +63,9 @@ def stop(service_name):
     core.state[service_name.started-service] is used to determine if we started
     the service. After shutdown, this is set to False.
 
+    If `force` is True, ignore the above flag and always stop the service.
     """
-    if not core.state.get(service_name + '.started-service'):
+    if not force and not core.state.get(service_name + '.started-service'):
         core.skip('did not start service ' + service_name)
         return
 
@@ -73,12 +76,12 @@ def stop(service_name):
     core.check_system(command, 'Stop ' + service_name + ' service')
     core.state[service_name + '.started-service'] = False
 
-def check_stop(service_name, timeout=10):
+def check_stop(service_name, timeout=10, force=False):
     """
     Stop a service, 'service_name' via init script or systemd and ensure that it
     stops running within a 'timeout' second window (default=10s)
     """
-    stop(service_name)
+    stop(service_name, force)
     assert is_stopped(service_name, timeout=timeout), "%s is still running" % service_name
 
 def status(service_name):
