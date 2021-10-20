7e53c955cc102b2acd7eab69e786977a22857fbd
Matyas Selmeci <matyas@cs.wisc.edu>
2021-10-20 16:12:07 -0500

    Refactor scitokens stuff into a separate library and request scitokens for xrootd
    Create a "library/credentials.py" library that contains functions for requesting and manipulating credentials (currently scitokens but in the future also idtokens and macaroons); modify test_401_scitokens.py to use this new library.
    
    The primary function in credentials is request_scitoken() which requests a scitoken from demo.scitokens.org and sets values in the core.state and core.config dict appropriately.  Created tokens are cleaned up in special_cleanup.py.
    
    core.state['token.NAME_created'] now really means 'did we create this token' instead of 'does this token exist'; condor tests now use os.path.exists() instead.

--- osgtest/tests/test_401_scitokens.py
+++ osgtest/tests/test_401_scitokens.py
@@ -1,61 +1,52 @@
-import json
-import os
-import pwd
-import time
+from urllib import error
 
-from urllib import error, request
-
-import osgtest.library.core as core
-import osgtest.library.files as files
-import osgtest.library.osgunittest as osgunittest
-
-# Headers so that heroku doesn't block us
-HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko)' +
-                         'Chrome/35.0.1916.47 Safari/537.36',
-           'Content-Type': 'application/json'}
-
-
-def request_demo_scitoken(scope, audience='ANY'):
-    """Request a token with 'scope' from the demo SciTokens issuer
-    """
-
-    payload_dict = {'aud': audience,
-                    'ver': 'scitokens:2.0',
-                    'scope': scope,
-                    'exp': int(time.time() + 3600),
-                    'sub': 'osg-test'}
-    payload = json.dumps({'payload': payload_dict,
-                          'algorithm': 'ES256'}).encode()
-
-    req = request.Request('https://demo.scitokens.org/issue',
-                          data=payload,
-                          headers=HEADERS)
-
-    return request.urlopen(req).read()
+from ..library import core, credentials, osgunittest
 
 
 class TestTokens(osgunittest.OSGTestCase):
+    def test_00_setup(self):
+        for token_name in ["condor_write", "xrootd", "xrootd_tpc_1", "xrootd_tpc_2"]:
+            credentials.reserve_scitoken(token_name)
 
     def test_01_request_condor_write_scitoken(self):
-        core.state['token.condor_write_created'] = False
-        core.config['token.condor_write'] = '/tmp/condor_write.scitoken'
-
         core.skip_ok_unless_installed('htcondor-ce', 'condor')
         self.skip_ok_if(core.PackageVersion('condor') <= '8.9.4',
                         'HTCondor version does not support SciToken submission')
-        self.skip_ok_if(os.path.exists(core.config['token.condor_write']),
-                        'SciToken with HTCondor WRITE already exists')
-
         hostname = core.get_hostname()
+        credentials.request_scitoken(
+            "condor_write",
+            scope="condor:/READ condor:/WRITE",
+            audience=f"{hostname}:9619",
+            overwrite=True
+        )
+
+    def test_02_request_xrootd_scitokens(self):
+        self.skip_ok_unless(core.config.get('xrootd.security') == "SCITOKENS",
+                            "Not using SciTokens for XRootD")
         try:
-            token = request_demo_scitoken('condor:/READ condor:/WRITE', audience=f'{hostname}:9619')
+            credentials.request_scitoken(
+                "xrootd",
+                subject=core.options.username,
+                scope="write:/",
+                audience="OSG_TEST",
+                overwrite=True,
+                log=True,
+            )
+            credentials.request_scitoken(
+                "xrootd_tpc_1",
+                subject=core.options.username,
+                scope="read:/",
+                audience="OSG_TEST",
+                overwrite=True,
+                log=True,
+            )
+            credentials.request_scitoken(
+                "xrootd_tpc_2",
+                subject=core.options.username,
+                scope="write:/",
+                audience="OSG_TEST",
+                overwrite=True,
+                log=True,
+            )
         except error.URLError as exc:
             self.fail(f"Failed to request token from demo.scitokens.org:\n{exc}")
-
-        ids = (0, 0)
-        if core.state['user.verified']:
-            user = pwd.getpwnam(core.options.username)
-            ids = (user.pw_uid, user.pw_gid)
-
-        files.write(core.config['token.condor_write'], core.to_str(token), backup=False, chown=ids)
-        core.state['token.condor_write_created'] = True
